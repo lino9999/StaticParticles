a2eb1011745a73e9b635d670aa15a7d5412dd084
Lino <lino9999.123@gmail.com>
2025-07-27 19:47:51 +0200

    Initial commit

---
+++ src/main/java/com/Lino/staticParticles/StaticParticles.java
@@ -0,0 +1,17 @@
+package com.Lino.staticParticles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaticParticles extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
